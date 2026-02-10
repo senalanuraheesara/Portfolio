6ed80a5ca81952522baa3af3c1f640f20fb068c6
Senal Anuraheesaera <senalanuraheesara@gmail.com>
2026-02-11 00:06:14 +0530

    Create Skills.jsx

---
+++ src/Pages/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center text-white">
+            <h1 className="text-4xl font-bold">Skills Page</h1>
+        </div>
+    );
+};
+
+export default Skills;
