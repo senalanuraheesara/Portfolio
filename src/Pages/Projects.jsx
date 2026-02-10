4195c5582f130134330836e4cc6dae259af89b17
Senal Anuraheesaera <senalanuraheesara@gmail.com>
2026-02-11 00:44:49 +0530

    Create Projects.jsx

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center text-white">
+            <h1 className="text-4xl font-bold">Projects Page</h1>
+        </div>
+    );
+};
+
+export default Projects;
